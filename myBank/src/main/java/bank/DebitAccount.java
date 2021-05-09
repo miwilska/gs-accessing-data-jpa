86003dfa4eb6cc73c663ede8f8080fc7cca89275
miwilska <22717834+miwilska@users.noreply.github.com>
2021-05-09 16:12:46 +0200

    myBank version - adding rest branch (by coping?)

---
+++ myBank/src/main/java/bank/DebitAccount.java
@@ -0,0 +1,22 @@
+package bank;
+
+import javax.persistence.Entity;
+
+/**
+ * Created by prubac on 4/15/2016.
+ */
+@Entity
+public class DebitAccount extends Account {
+
+    public DebitAccount() {
+        super();
+    }
+
+    public DebitAccount(Long accountID, Customer customer) {
+        super(accountID, customer);
+    }
+
+    public DebitAccount(Customer customer) {
+        super(customer);
+    }
+}
