668a1055c46abc8828695c6268b60a535737c701
miwilska <22717834+miwilska@users.noreply.github.com>
2021-05-19 20:16:47 +0200

    my version of name of client

--- myBank/src/main/java/bank/Application.java
+++ myBank/src/main/java/bank/Application.java
@@ -21,18 +21,18 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
-	//@Bean
+	@Bean
 	public CommandLineRunner demo(CustomerRepository repository, AccountRepository accountRepository) {
 		return (args) -> {
 			// save a couple of customers
 
-			Customer c1 = new Customer("Jack", "Bauer");
-			Customer c2 = new Customer("Chloe", "O'Brian");
+			Customer c1 = new Customer("Jan", "Kowalski");
+			Customer c2 = new Customer("Stanislaw", "Nowak");
 			repository.save(c1);
 			repository.save(c2);
-			repository.save(new Customer("Kim", "Bauer"));
-			repository.save(new Customer("David", "Palmer"));
-			repository.save(new Customer("Michelle", "Dessler"));
+			repository.save(new Customer("Maria", "Kowalska"));
+			repository.save(new Customer("Dawid", "Pipsztycki"));
+			repository.save(new Customer("Michalina", "Dare"));
 
 			accountRepository.save(new SavingsAccount(c1));
 			accountRepository.save(new DebitAccount(c1));
